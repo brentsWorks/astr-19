a3a8df601354854093c35d6033d7f16a3bda32b0
Brent <134455660+brentsWorks@users.noreply.github.com>
2024-01-09 12:53:09 -0800

    Session 1 prompt for Coding Journal 1.

---
+++ cj1-1.py
@@ -0,0 +1,12 @@
+# Brent Brison
+# Doctor Aguichine
+# ASTR 19
+# 6 January 2023
+
+print("Name: Brent Brison")
+print("Pronouns: He/Him\n")
+print("My favorite movie is Chasing Mavericks, which actually is based in Half Moon Bay,",end="")
+print("and details the true story of a surfer named Jay Moriarity, and his journey to conquer ",end="")
+print("the infamous wave known as Mavericks.\n")
+print("Although arguable, my favorite food is perhaps a nice meal of Steak and Mashed Potatoes.",end="")
+print("Of course that can come from varying places or, at best, from home.")
