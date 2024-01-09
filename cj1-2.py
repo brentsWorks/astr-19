befc7c2d113ff3c957693ef1443114a54114efab
Brent <134455660+brentsWorks@users.noreply.github.com>
2024-01-09 12:53:27 -0800

    Section 2 prompt for Coding Journal 1.

---
+++ cj1-2.py
@@ -0,0 +1,19 @@
+# Brent Brison
+# Doctor Aguichi
+# ASTR 19
+# 9 January 2023
+
+
+
+float1 = 1.2
+float2 = 3.5
+int1 = 6
+int2 = 3
+
+print("Float var 1:", float1, "\nFloat var 2:", float2)
+print("Sum of float 1 and float 2:", float1+float2,"\nType:", type(float1+float2), "\n")
+
+print("Int var 1:", int1, "\nInt var 2:", int2)
+print("Difference of int 1 and int 2:",int1-int2, "\nType:", type(int1-int2), "\n")
+
+print("Product of float 1 and int 1:", float1*int1, "\nType:", type(float1*int1))
